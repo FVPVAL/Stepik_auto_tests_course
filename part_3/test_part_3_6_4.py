df1779cf3d6fa5801fe81ca82bc7cd64596e4f8a
val <izol@rambler.ru>
2024-04-24 13:03:45 +0300

    Добавил conftest.py и test_part_3_6_4.py

---
+++ part_3/test_part_3_6_4.py
@@ -0,0 +1,13 @@
+from selenium.webdriver.common.by import By
+import time
+
+link = "https://stepik.org/lesson/236895/step/1"
+
+
+def test_authorization(browser):
+    browser.get(link)
+    browser.find_element(By.ID, "ember453").click()
+    browser.find_element(By.ID, "id_login_email").send_keys("izol@rambler.ru")
+    browser.find_element(By.ID, "id_login_password").send_keys("Qwet5j")
+    browser.find_element(By.CSS_SELECTOR, ".button_with-loader").click()
+    time.sleep(5)
